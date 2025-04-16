e6deaf183f2667d03ba4d5057ae8bb816e3fa3d1
maneesh0021 <maneesh.bachu@zemosolabs.com>
2025-04-16 09:58:27 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,14 +1,13 @@
 function processUserInput(name, callback) {
-    console.log("Processing user input...");
-    callback(name);
+   
+     callback(name);
 }
 
 function greetUser(name) {
-    console.log(`Hello, ${name}!`);
+    
 }
 
 processUserInput("Maneesh", greetUser);
 
 
 const getInitials = (firstName, lastName) => firstName[0] + lastName[0];
-console.log(getInitials("Roger", "Waters")); 
