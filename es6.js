b7fcb1dcb5bc8f8bc454318ebf9e0412e0dd103a
maneesh0021 <maneesh.bachu@zemosolabs.com>
2025-04-16 09:56:49 +0530

    Update es6.js

--- es6.js
+++ es6.js
@@ -1,16 +1,13 @@
-const printName = name => `Hi${name}`;
-console.log(printName("Maneesh"))
+ const printName = name => `Hi${name}`;
+printName("Maneesh");
 
 const printBill = (name, bill) => `Hi ${name}, please pay: ${bill}`;
-console.log(printBill("Maneesh",500))
+printBill("Maneesh", 500);
 
 const person = {
-    name: "Noam Chomsky",
-    age: 92
-  };
-  const { name, age } = person;
-  console.log(name);
-  console.log(age);
-  
-  
-  
+  name: "Noam Chomsky",
+  age: 92
+};
+const { name, age } = person;
+name;
+age;
