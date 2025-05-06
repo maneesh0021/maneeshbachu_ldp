ab34bdc5b68531da6be508134ab9a15f34623d6f
maneeshbachu <maneesh.bachu@zemosolabs.com>
2025-05-06 17:06:15 +0530

    feat-updated all the files

--- React-Pair Program/pair-programming1/src/components/molecules/IconLabel/index.tsx
+++ React-Pair Program/pair-programming1/src/components/molecules/IconLabel/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {  Box } from "@mui/material";
+import { Box } from "@mui/material";
 import Icon from "../../atoms/Icon";
 import CustomTypography from "../../atoms/Typography";
 
